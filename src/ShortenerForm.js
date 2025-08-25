b5c613a3cf6a47adbafe2d7a4e1c9d9313c371af
Adiii581 <ac.aditya123@gmail.com>
2025-08-24 22:55:01 -0400

    Update ShortenerForm.js

--- src/ShortenerForm.js
+++ src/ShortenerForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import axios from 'axios'; // Import axios
+import axios from 'axios';
 
 function ShortenerForm() {
   const [url, setUrl] = useState('');
@@ -15,10 +15,15 @@ function ShortenerForm() {
     setShortUrl(null);
     setError(null);
 
+    // This is the line that needs to be updated for deployment.
+    // It will use the environment variable you set in Vercel.
+    // When running locally, it will fall back to localhost:5000 if the variable isn't set.
+    const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+
     try {
-      // Make the POST request to the Flask backend
-      const apiUrl = process.env.REACT_APP_API_URL;
-      const response = await axios.post(`${apiUrl}/api/shorten`, { ... });
+      const response = await axios.post(`${apiUrl}/api/shorten`, {
+        url: url // This is the correct request body
+      });
       setShortUrl(response.data.short_url);
     } catch (err) {
       setError('Could not shorten the URL. Please try again.');
